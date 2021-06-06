90cab8a290dc4e65482e60453527140bd698ce27
AhmadShalein <ahmadshalein@gmail.com>
2021-06-06 17:56:06 +0300

    lab 01 has done

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+    render(){
+      return(
+        <div>
+            <footer>@copy copyrights 2021 made by: Ahmad Shalein</footer>
+        </div>
+      )
+    }
+  }
+  
+  export default Footer;
---
+++ src/components/HornedBeasts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeasts extends React.Component {
+    render(){
+      return(
+        <div>
+           <h2></h2>
+           <p></p>
+           <img src="" alt="HornedBeast" title="" />
+        </div>
+      )
+    }
+  }
+  
+  export default HornedBeasts;
