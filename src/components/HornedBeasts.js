abfad3b0881a8334581fceafb0ecda625827e306
AhmadShalein <ahmadshalein@gmail.com>
2021-06-07 13:37:27 +0300

    lab 01

--- src/components/HornedBeasts.js
+++ src/components/HornedBeasts.js
@@ -4,9 +4,9 @@ class HornedBeasts extends React.Component {
     render(){
       return(
         <div>
-           <h2></h2>
-           <p></p>
-           <img src="" alt="HornedBeast" title="" />
+           <h2 className='title'>{this.props.title}</h2>
+           <img src={this.props.url} alt={this.props.discription} title={this.props.title} />
+           <p className='discription'>{this.props.discription}</p>
         </div>
       )
     }
